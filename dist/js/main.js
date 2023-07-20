95c3b608a92f5e3505f4c9e6ae82ed35d19d2345
canderson9625 <canderson9625@gmail.com>
2023-07-19 20:16:33 -0400

    add react support, portfolio v1

--- dist/js/main.js
+++
@@ -1,2 +0,0 @@
-"use strict";let user={name:"test",prop:3};
-//# sourceMappingURL=main.js.map
--- server.js
+++ server.js
@@ -1,28 +1,45 @@
-const express = require('express')
+// CJS
+// const express = require('express')
+// const app = express();
+// const port = 8080;
+// const path = require('path');
+
+// const fs = require('fs');
+// const sharp = require('sharp');
+
+// const liveReload = require('livereload');
+// const connectLiveReload = require('connect-livereload');
+
+import express from 'express';
 const app = express();
 const port = 8080;
-const path = require('path');
+import * as path from 'path';
+import { execSync } from 'child_process';
 
-const fs = require('fs');
-const sharp = require('sharp');
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const liveReload = require('livereload');
-const connectLiveReload = require('connect-livereload');
+import fs from 'fs';
+import sharp from 'sharp';
+
+import liveReload from 'livereload';
+import connectLiveReload from 'connect-livereload';
 
 async function build () {
-    console.log('building');
+    // console.log('building');
+    
     // Convert images to webp
     fs.readdir(path.resolve(__dirname + '/src/media'), (err, files) => {
         if (err) { throw err; }
-        files.forEach(file => {
-            sharp(path.resolve(__dirname + '/src/media/' + file))
-                .toFormat('webp')
-                .toFile(path.resolve(__dirname + '/dist/media/' + file + '.webp'));
-        });
+        // files.forEach(file => {
+        //     sharp(path.resolve(__dirname + '/src/media/' + file))
+        //         .toFormat('webp')
+        //         .toFile(path.resolve(__dirname + '/dist/assets/media/' + file + '.webp'));
+        // });
     });
 
     // execSync('npm run build', {stdio: 'inherit'});
-    require('child_process').execSync('npm run build');
+    execSync('npm run build');
 }
 
 new Promise(async () => { 
